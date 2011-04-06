41f03d73c7bbd4b0549966820fe78a9e4dd57ec7
That Girl <codegrl@gmail.com>
2011-04-06 09:12:54 -0400

    now with more url constructing

--- lilliputian.py
+++ lilliputian.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python2
 
 import sys
-import fileinput
 import markdown
+import time
+from datetime import date
+
+filename = str(date.today())
 
 markdown.markdownFromFile(input=sys.argv[1],
-                          output="test.html",
+                          output="/home/laura/public_html/blog/" + filename + ".html",
                           encoding="utf8")
+
